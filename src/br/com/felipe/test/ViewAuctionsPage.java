e00055aeb256287266e7771e648dac64ba14483a
Felipe Rodrigues <3rd.luis.rodrigues@almundo.com>
2018-09-27 17:30:19 -0300

    add explicit wait

--- src/br/com/felipe/test/ViewAuctionsPage.java
+++ src/br/com/felipe/test/ViewAuctionsPage.java
@@ -3,7 +3,9 @@ package br.com.felipe.test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ViewAuctionsPage {
 
@@ -24,13 +26,13 @@ public class ViewAuctionsPage {
 
     public boolean bidValidate(String name, double value){
 
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
-        return  driver.getPageSource().contains(name) &&
-                driver.getPageSource().contains(String.valueOf(String.valueOf(value)));
+        boolean containsUser = new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.textToBePresentInElement(By.id("lancesDados"), name));
+
+        if(containsUser) return driver.getPageSource().contains(String.valueOf(String.valueOf(value)));
+
+        return false;
+
     }
 }
